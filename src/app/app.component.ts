31b01c0db8dba4448a8593f18c75170e68b10fb9
Andrew Lo <andrew.lo@rangle.io>
2017-04-15 15:55:49 -0400

    Highlight side menu item when corresponding route is active

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,13 +5,33 @@ import { NotificationService } from './core/notification.service';
 import { NavigationService } from './core/navigation.service';
 
 import { Notification } from './types/notification';
+import { SideMenuItems } from './types/side-menu-items';
 
 @Component({
   selector: 'al-app',
   templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   sideMenuOpen = false;
+
+  sideMenuItems: SideMenuItems[] = [{
+    route: '',
+    label: 'Home',
+    icon: 'fa-home',
+  }, {
+    route: 'login',
+    label: 'Login',
+    icon: 'fa-sign-in',
+  }, {
+    route: 'list',
+    label: 'List',
+    icon: 'fa-list',
+  }, {
+    route: 'settings',
+    label: 'Settings',
+    icon: 'fa-cog',
+  }];
 
   constructor(
     private notificationService: NotificationService,
---
+++ src/app/types/side-menu-items.ts
@@ -0,0 +1,5 @@
+export interface SideMenuItems {
+  icon: string;
+  label: string;
+  route: string;
+}
